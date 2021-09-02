4f21a991aad35b22cd4488db8c63458aef6e4c73
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-03 08:50:33 +0900

    🎨 CustomHooksの使用

---
+++ src/hooks/useInputArray.jsx
@@ -0,0 +1,26 @@
+﻿import { useCallback, useState } from "react";
+
+export const useInputArray = () => {
+    const [text, setText] = useState("");
+    const [array, setArray] = useState([]);
+
+    const handleChange = useCallback((e) => {
+        if (e.target.value.length > 5) {
+            alert("warning");
+            return;
+        }
+        setText(e.target.value.trim());
+    }, []);
+
+    const handleAdd = useCallback(() => {
+        setArray((prevArray) => {
+            if (prevArray.some((item) => item === text)) {
+                alert("同じテキストが存在します");
+                return prevArray;
+            }
+            return [...prevArray, text];
+        });
+    }, [text]);
+
+    return { array, text, handleChange, handleAdd };
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,51 +1,16 @@
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-    const [count, setCount] = useState(1);
-    const [text, setText] = useState("");
-    const [isShow, setIsShow] = useState(true);
-    const [array, setArray] = useState([]);
-
-    const handleClick = useCallback(() => {
-        if (count < 10) {
-            setCount((prevCount) => prevCount + 1);
-        }
-    }, [count]);
-
-    const handleDisplay = useCallback(() => {
-        setIsShow((prevIsShow) => !prevIsShow);
-    }, []);
-    
-    const handleChange = useCallback((e) => {
-        if (e.target.value.length > 5) {
-            alert("warning");
-            return;
-        }
-        setText(e.target.value.trim());
-    }, []);
-
-
-    const handleAdd = useCallback(() => {
-        setArray((prevArray) => {
-            if (prevArray.some((item) => item === text)) {
-                alert("同じテキストが存在します");
-                return prevArray;
-            }
-            return [...prevArray, text];
-        });
-    }, [text]);
-
-    useEffect(() => {
-        document.body.style.backgroundColor = "lightblue";
-        return () => {
-            document.body.style.backgroundColor = "";
-        };
-    }, []);
+    const { count, isShow, handleClick, handleDisplay } = useCounter();
+    const { array, text, handleChange, handleAdd } = useInputArray();
+    useBgLightBlue();
 
     return (
         <div className={styles.container}>
