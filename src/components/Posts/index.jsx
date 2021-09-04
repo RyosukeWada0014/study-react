be5158d834c50e582ecb1d0dd6d6190f147994c0
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-04 21:14:41 +0900

    ⚡️ useReducerの使用

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,47 +1,60 @@
-﻿import { useCallback, useEffect, useState } from "react";
+﻿import { useCallback, useEffect, useReducer, useState } from "react";
+
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return { ...state, data: action.data, loading: false };
+    case "error":
+      return { ...state, loading: false, error: action.error };
+    default: {
+      throw new Error("no such action type");
+    }
+  }
+};
 
 export const Posts = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const getPosts = useCallback(async () => {
-        try {
-            const res = await fetch(
-                "https://jsonplaceholder.typicode.com/posts"
-            );
-            if (!res.ok) {
-                throw new Error("エラーが発生しました");
-            }
-            const json = await res.json();
-            setPosts(json);
-        } catch (error) {
-            setError(error);
-        }
-        setLoading(false);
-    }, []);
-
-    useEffect(() => {
-        getPosts();
-    }, [getPosts]);
-
-    if (loading) {
-        return <div>ロード中</div>;
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("エラーが発生しました");
+      }
+      const json = await res.json();
+      dispatch({ type: "end", data: json });
+    } catch (error) {
+      dispatch({ type: "error", error });
     }
+  }, []);
 
-    if (error) {
-        <div>{error.message}</div>;
-    }
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
-    if (posts.length === 0) {
-        <div>データがありません</div>;
-    }
+  if (state.loading) {
+    return <div>ロード中</div>;
+  }
 
-    return (
-        <ol>
-            {posts.map((post) => {
-                return <li key={post.id}>{post.title}</li>;
-            })}
-        </ol>
-    );
+  if (state.error) {
+    return <div>{state.error.message}</div>;
+  }
+
+  if (state.data.length === 0) {
+    return <div>データがありません</div>;
+  }
+
+  return (
+    <ol>
+      {state.data.map((post) => {
+        return <li key={post.id}>{post.title}</li>;
+      })}
+    </ol>
+  );
 };
