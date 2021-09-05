da86085ca0430ce622b4c3910109fb30e75a785f
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-05 20:51:14 +0900

    🚚 ファイルシステムルーティングを試す

---
+++ src/pages/post/[id]/index.jsx
@@ -0,0 +1,21 @@
+﻿import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { useRouter } from "next/router";
+
+import { Header } from "src/components/Header";
+
+const PostId = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Index Page</title>
+      </Head>
+      <Header />
+      <div>{router.query.id}</div>
+    </div>
+  );
+};
+
+export default PostId;
