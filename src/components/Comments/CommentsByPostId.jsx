90247a60154737abab2abed5a81da27e29296b07
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-07 10:45:17 +0900

    ✨ 投稿の詳細ページにコメントの詳細を表示

---
+++ src/components/Comments/CommentsByPostId.jsx
@@ -0,0 +1,34 @@
+﻿import Link from "next/link";
+import { useCommentsByPostId } from "src/hooks/useFetchArray";
+
+export const CommentsByPostId = (props) => {
+  const { data, error, isLoading, isEmpty } = useCommentsByPostId(props.id);
+
+  if (isLoading) {
+    return <div>ロード中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    return <div>データがありません</div>;
+  }
+
+  return (
+    <div>
+      <ol>
+        {data.map((comment) => {
+          return (
+            <li key={comment.id}>
+              <Link href={`comments/${comment.id}`}>
+                <a>{comment.body}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -17,9 +17,15 @@ const API_URL = "https://jsonplaceholder.typicode.com";
 export const useComments = () => {
   return useFetchArray(`${API_URL}/comments`);
 };
+
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`);
 };
+
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
+};
+
+export const useCommentsByPostId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
 };
